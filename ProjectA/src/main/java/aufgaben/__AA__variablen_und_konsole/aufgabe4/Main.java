dbe8a2f4bf13aa35c8b75740a1b9049899c445c5
Jemal Schleicher <jemal.schleicher@cgi.com>
2025-10-22 10:59:22 +0200

    #1: Variablen und Konsolen Aufgabenpakete

---
+++ ProjectA/src/main/java/aufgaben/__AA__variablen_und_konsole/aufgabe4/Main.java
@@ -0,0 +1,19 @@
+package aufgaben.__AA__variablen_und_konsole.aufgabe4;
+
+public class Main {
+  public static void main(String[] args) {
+    boolean a = true;
+    boolean b = true;
+    boolean c = true;
+    int number = 0;
+
+    boolean condition1 = a || !b;
+    boolean condition2 = c && number == (int) (6.48074069841 * 6.48074069841);
+    boolean condition3 = !(a && c);
+    boolean condition4 = number % 2 == 0 && number > 40;
+
+    boolean solution = condition1 && condition2 && condition3 && condition4;
+
+    System.out.println("Solution: " + solution);
+  }
+}
