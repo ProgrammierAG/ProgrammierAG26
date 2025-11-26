2c95b8951510e8d355f6435dbf83b03832450e69
NikBomma <nikbomma@gmail.com>
2025-11-26 15:26:21 +0100

    Java Übungen 1:

--- JavaTraining/src/main/java/programmierag/aufgaben/__01__variablen_und_konsole/aufgabe2/Main.java
+++ JavaTraining/src/main/java/programmierag/aufgaben/__01__variablen_und_konsole/aufgabe2/Main.java
@@ -4,8 +4,13 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+    System.out.print("Gib deine Lieblingsfarbe ein: ");
     Scanner sc = new Scanner(System.in);
-
-    sc.close();
+    String Lieblingsfarbe = sc.nextLine();
+    System.out.println("Deine lieblingsfarbe ist -> " + Lieblingsfarbe);
+    System.out.print("Gib deine Lieblingszahl ein: ");
+    Scanner sc2 = new Scanner(System.in);
+    int Lieblingszahl = sc2.nextInt();
+    System.out.println("Deine lieblingszahl ist -> " + Lieblingszahl);
   }
 }
