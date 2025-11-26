3c281d151b39e20f39785fed2a1e71093e7a718a
PMHG <sophia.sbrandl@gmail.com>
2025-11-26 15:30:08 +0100

    task bis 4

--- JavaTraining/src/main/java/programmierag/aufgaben/__01__variablen_und_konsole/aufgabe3/Main.java
+++ JavaTraining/src/main/java/programmierag/aufgaben/__01__variablen_und_konsole/aufgabe3/Main.java
@@ -2,21 +2,21 @@ package programmierag.aufgaben.__01__variablen_und_konsole.aufgabe3;
 
 public class Main {
   public static void main(String[] args) {
-//    int 1zahl = 10;
-//    double preis = "19.99";
-//    boolean istFertig = "true";
-//    char zeichen = "Z";
-//    String name123 = 456;
-//    float piWert = 3.1415926535;
-//    long großeZahl = 999999999999999999999999L;
-//    int alter! = 30;
-//    boolean istStudent = Ja;
-//    char = 'A';
-//    String *vorname = 'Max';
-//    int zahl mitLeerzeichen = 100;
-//    float f = 3.14;
-//    boolean aktiv = 1;
-//    char smiley = ':)';
-//    String $nachname = true;
+    int zahl = 10;
+    double preis = 19.99;
+    boolean istFertig = true;
+    char zeichen = 'Z';
+    String name123 = "456";
+    float piWert = 3.1415926535f;
+    long grosseZahl = 99999999999999999L;
+    int alter = 30;
+    boolean istStudent = true;
+    char hihi= 'A';
+    String vorname = "Max";
+    int zahlmitLeerzeichen = 100;
+    float f = 3.14f;
+    boolean aktiv = true;
+    char smiley = ':';
+    String $nachname = "true";
   }
 }
