809852b4b265629976db5ded29c340952379912c
PMHG <sophia.sbrandl@gmail.com>
2025-11-26 15:30:08 +0100

    task bis 4

--- java_training/src/main/programmierag/aufgaben/__01__variablen_und_konsole/aufgabe2/Main.java
+++ java_training/src/main/programmierag/aufgaben/__01__variablen_und_konsole/aufgabe2/Main.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-
+    System.out.println("Gib deine Lieblingsfarbe an: ");
+    String farbe = sc.nextLine();
+    System.out.println("Deine lieblingsfarbe ist -> " + farbe);
+    System.out.println("Gib deine Lieblingszahl an: ");
+    String zahl = sc.nextLine();
+    System.out.println("Deine lieblingszahl ist ->" + zahl);
     sc.close();
   }
 }
