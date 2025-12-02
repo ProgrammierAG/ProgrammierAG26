7f5410ce4198c329b3aaf26c23f9d8e4796e9fa0
Jemal Schleicher <jemal.schleicher@cgi.com>
2025-12-02 13:55:48 +0100

    chore: updated gradle dependencies

--- JavaTraining/build.gradle.kts
+++ JavaTraining/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("java")
-    kotlin("jvm") version "2.2.0"
 }
 
 group = "programmierag"
@@ -8,17 +7,4 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation(kotlin("stdlib-jdk8"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(21)
 }
