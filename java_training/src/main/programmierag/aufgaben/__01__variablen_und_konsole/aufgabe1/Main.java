1fbfcf9fd19ed7a61985d252e95cd5f8efd51543
unknown <JasimShahulahmeed>
2025-12-10 15:18:21 +0100

    task:Aufgabe 1,2 & 3 bearbeitet

--- java_training/src/main/programmierag/aufgaben/__01__variablen_und_konsole/aufgabe1/Main.java
+++ java_training/src/main/programmierag/aufgaben/__01__variablen_und_konsole/aufgabe1/Main.java
@@ -2,6 +2,6 @@ package programmierag.aufgaben.__01__variablen_und_konsole.aufgabe1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World.Ich bin Jasim");
     }
 }
--- java_training/src/main/programmierag/aufgaben/__01__variablen_und_konsole/aufgabe2/Main.java
+++ java_training/src/main/programmierag/aufgaben/__01__variablen_und_konsole/aufgabe2/Main.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-
+    System.out.println("Was ist deine Lieblingsfarbe?");
+    String farbe =sc.nextLine();
+    System.out.println("Deine Lieblingsfarbe ist ->" + farbe+"!");
+    System.out.println("Was ist deine Lieblingszahl?");
+    int zahl =sc.nextInt();
+    System.out.println("Deine Lieblingszahl ist ->" + zahl+"!");
     sc.close();
   }
 }
