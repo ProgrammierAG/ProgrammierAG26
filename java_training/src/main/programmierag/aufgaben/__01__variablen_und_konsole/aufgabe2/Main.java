73368c2714ea642e29587715d1491698376f2e02
Tu-0819 <shitu.wang@yahoo.com>
2025-12-10 15:23:49 +0100

    Bearbeitung Java-Aufgaben.

--- java_training/src/main/programmierag/aufgaben/__01__variablen_und_konsole/aufgabe2/Main.java
+++ java_training/src/main/programmierag/aufgaben/__01__variablen_und_konsole/aufgabe2/Main.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+    System.out.println("Gebe deine Lieblingsfarbe ein: ");
     Scanner sc = new Scanner(System.in);
-    String lieblingsfarbe = "pink";
+    String lieblingsfarbe = sc.nextLine();
     System.out.println("Deine Lieblingsfarbe ist --> " + lieblingsfarbe);
+    System.out.println("Gebe deine Lieblingszahl ein: ");
+    String lieblingszahl = sc.nextLine();
+    System.out.println("Deine Lieblingszahl ist --> " + lieblingszahl);
     sc.close();
   }
 }
