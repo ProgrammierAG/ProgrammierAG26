ec24dcb273d23288bdf9d5dbac9df1b678fd74d7
unknown <matthias.laber@gmx.net>
2025-12-17 14:58:32 +0100

    erster commit

---
+++ java_training/src/main/programmierag/aufgaben/__02__if_else_und_switch/aufgabe2/Aufgabe2.java
@@ -0,0 +1,29 @@
+package programmierag.aufgaben.__02__if_else_und_switch.aufgabe2;
+
+/*
+Schreibe ein Programm das eine Zahl, in ihre Note umwandelt benutze ein switch case dafür.
+
+        - 1 : "sehr gut"
+        - 2 : "gut"
+        - 3 : "befriedigend"
+        - 4 : "ausreichend"
+        - 5 : "mangelhaft"
+        - 6 : "ungenügend"
+*/
+
+public class Aufgabe2 {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
